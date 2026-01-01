258d5bec45cdadcb17fcca04f71920f6a2b971aa
Fu_Manifold <12512619@mail.sustech.edu.cn>
2026-01-01 16:00:41 +0800

    留档

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
-// @ts-check
-
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'; // 确保这一行有
 
-// https://astro.build/config
+// 1. 引入刚才下载的数学插件
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	// 这里改成你之前决定的 GitHub Page 地址
+	site: 'https://你的GitHub用户名.github.io',
+	base: '/tensorfieldx',
+
+	integrations: [
+		mdx(),
+		sitemap(),
+		tailwind(), // 启用 Tailwind CSS
+	],
+
+	markdown: {
+		// 2. 在这里告诉 Astro 处理 Markdown 时要把公式渲染出来
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 });
